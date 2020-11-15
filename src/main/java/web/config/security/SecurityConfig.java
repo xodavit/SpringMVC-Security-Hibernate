216d8421420f37d2ad7542c2558cdb9dc125e82f
vitaliy_khodataev <khodataev.v@yandex.ru>
2020-11-15 09:28:16 +0300

    import

--- src/main/java/web/config/security/SecurityConfig.java
+++ src/main/java/web/config/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import web.config.handler.LoginSuccessHandler;
